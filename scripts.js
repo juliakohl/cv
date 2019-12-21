587e1ba5a2d48e29dda33f6b6716bd00b04adaa2
Julia Kohl <julia.helmine.kohl@gmail.com>
2019-12-21 13:59:26 +0100

    add content and minor style changes

--- scripts.js
+++ scripts.js
@@ -2,23 +2,29 @@ var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 
 if(viewportWidth < 876){
 	document.querySelector("#frame").classList.add("mobile");
+	document.querySelector("h1").classList.add("mobile");
 	document.querySelector(".nav").style.display = "none";
 	document.querySelector(".mnav").style.display = "flex";
 	document.querySelector(".credits").classList.add("no-display");
+	document.querySelector(".copyright").classList.add("no-display");
 }
 
 window.addEventListener('resize', function () {
 	viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 	if (viewportWidth >= 877) {
 		document.querySelector("#frame").classList.remove("mobile");
+		document.querySelector("h1").classList.remove("mobile");
 		document.querySelector(".nav").style.display = "flex";
 		document.querySelector(".mnav").style.display = "none";
 		document.querySelector(".credits").classList.remove("no-display");
+		document.querySelector(".copyright").classList.remove("no-display");
 	} else {
 		document.querySelector("#frame").classList.add("mobile");
+		document.querySelector("h1").classList.add("mobile");
 		document.querySelector(".nav").style.display = "none";
 		document.querySelector(".mnav").style.display = "flex";
 		document.querySelector(".credits").classList.add("no-display");
+		document.querySelector(".copyright").classList.add("no-display");
 	}
 }, false);
 
