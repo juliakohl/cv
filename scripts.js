64cb449733a92c3b88788f2906b538d2519806b7
Julia Kohl <julia.helmine.kohl@gmail.com>
2019-12-21 14:24:25 +0100

    adjust mobile nav

--- scripts.js
+++ scripts.js
@@ -28,13 +28,13 @@ window.addEventListener('resize', function () {
 	}
 }, false);
 
-if(viewportWidth < 768){
+if(viewportWidth < 992){
     document.querySelector(".layout-1-2").classList.add("sm");
 }
 
 window.addEventListener('resize', function () {
 	viewportWidth = window.innerWidth || document.documentElement.clientWidth;
-	if (viewportWidth >= 768) {
+	if (viewportWidth >= 992) {
         document.querySelector(".layout-1-2").classList.remove("sm");
 	} else {
         document.querySelector(".layout-1-2").classList.add("sm");
